01f3f090d0d14f76b870326ea205f9109a53c998
Mohamed AlQadeery <moh9amad1@gmail.com>
2023-08-08 17:07:31 +0300

    add default values for variables in mapping config

--- MediumClone.Api/Common/Mappings/ArticlesMappingConfig.cs
+++ MediumClone.Api/Common/Mappings/ArticlesMappingConfig.cs
@@ -7,6 +7,10 @@ namespace MediumClone.Api.Common.Mappings;
 
 public class ArticlesMappingConfig : IRegister
 {
+    private const int _defaultPageNumber = 1;
+    private const int _defaultPageSize = 10;
+    private const string _defaultSortColumn = "id";
+    private const string _defaultSortOrder = "desc";
 
     public void Register(TypeAdapterConfig config)
     {
@@ -18,11 +22,11 @@ public class ArticlesMappingConfig : IRegister
 
 
         config.NewConfig<QueryParamters, CommonQueryParams>()
-            .Map(dest => dest.PageNumber, src => src.PageNumber ?? 1)
-            .Map(dest => dest.PageSize, src => src.PageSize ?? 10)
+            .Map(dest => dest.PageNumber, src => src.PageNumber ?? _defaultPageNumber)
+            .Map(dest => dest.PageSize, src => src.PageSize ?? _defaultPageSize)
             .Map(dest => dest.Search, src => src.Search ?? string.Empty)
-            .Map(dest => dest.SortColumn, src => src.SortColumn ?? "id")
-            .Map(dest => dest.SortOrder, src => src.SortOrder ?? "desc");
+            .Map(dest => dest.SortColumn, src => src.SortColumn ?? _defaultSortColumn)
+            .Map(dest => dest.SortOrder, src => src.SortOrder ?? _defaultSortOrder);
 
 
 
