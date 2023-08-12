d8b72863966f67978486b63bf8e775a5b76a5a72
Mohamed AlQadeery <moh9amad1@gmail.com>
2023-08-12 13:55:17 +0300

    tag compoarsion imporvment

--- MediumClone.Application/Articles/Queries/GetAllArticlesByTagQuery.cs
+++ MediumClone.Application/Articles/Queries/GetAllArticlesByTagQuery.cs
@@ -18,7 +18,7 @@ public class GetAllArticlesByTagQueryHandler : IRequestHandler<GetAllArticlesByT
     public async Task<PaginatedList<Article>> Handle(GetAllArticlesByTagQuery request, CancellationToken cancellationToken)
     {
         var query = _unitOfWork.Articles.GetQueryable().Include(a => a.ArticleTags)
-                    .Where(a => a.ArticleTags.Any(at => at.Tag.Name == request.Tag));
+                    .Where(a => a.ArticleTags.Any(at => at.Tag.Name.ToLower() == request.Tag.ToLower()));
 
         return await _unitOfWork.Articles.GetAllWithPaginationAsync(query, request.PageNumber, request.PageSize, new string[] { "Author" });
 
