aa31bb67b4d9393ff0e612c9c7f71ab54e15a82a
Mohamed AlQadeery <moh9amad1@gmail.com>
2023-08-22 00:46:58 +0300

    adding id in aritcle response

--- MediumClone.Api/Contracts/Articles/ArticleResponse.cs
+++ MediumClone.Api/Contracts/Articles/ArticleResponse.cs
@@ -3,7 +3,7 @@ using MediumClone.Application.Common;
 namespace MediumClone.Api.Contracts.Articles;
 
 public record AuthorResponse(string Id, string FullName, string Image);
-public record ArticleResponse(string Title, string Body, string Slug,
+public record ArticleResponse(int Id, string Title, string Body, string Slug,
  AuthorResponse Author, List<string> TagNames, DateTime CreatedDateTime);
 
 // public record GetAllArticlesResponse(PaginatedList<ArticleResponse> Articles);
